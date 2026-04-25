18513367397c345d84fccf768d9a11ca5502b37a
Jurgen <jurgen-kruja@live.com>
2026-04-25 14:11:42 +0200

    Initial commit

---
+++ backend/controllers/taskControllers.js
@@ -0,0 +1,21 @@
+const getTasks = (req, res) => {
+    res.status(200).json({ message: 'Get All Tasks' })
+}
+
+const setTask = (req, res) => {
+    if (!req.body || !req.body.text) {
+        res.status(400)
+        throw new Error('Please enter a text');
+    }
+    res.status(200).json({ message: 'Create task' })
+}
+
+const updateTask = (req, res) => {
+    res.status(200).json({ message: `Task ${req.params.id} updated.` })
+}
+
+const deleteTask = (req, res) => {
+    res.status(200).json({ message: `Task ${req.params.id} deleted.` })
+}
+
+module.exports = { getTasks, setTask, updateTask, deleteTask }
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+// Importojme dependencies
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+//Importojme middlewarey
+const {errorHandler}= require('./middlewares/errorMiddleware');
+
+//Krijojme nje varaibel per porten
+const port = process.env.PORT || 5000;
+
+//Inicializojme expreessin ne nje variabel app
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use('/api/tasks', require('./routes/taskRoutes'))
+
+//Middlewares
+app.use(errorHandler);
+
+// Inicializojme pritesin e requesteve
+app.listen(port, () => console.log(`Server is running on port ${port}`))
